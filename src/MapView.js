80adb733d28e5a9397db76d592f3a97f0e0454a7
Quentin Vuilliot <quentin.vuilliot@kaes.fr>
2015-07-10 18:14:56 +0200

    MapView is a simple OpenLayers OSM map for now

--- src/MapView.js
+++ src/MapView.js
@@ -2,6 +2,42 @@ var compose = require('ksf/utils/compose');
 var _ContentDelegate = require('absolute/_ContentDelegate');
 var Label = require('absolute/Label');
 
+var ol = require('openlayers');
+var VFlex = require('absolute/VFlex');
+var Element = require('absolute/Element');
+
+var Map = compose(_ContentDelegate, function() {
+	this._content = new Element;
+	this.olMap = new ol.Map({
+	  view: new ol.View({
+	    center: [0, 0],
+	    zoom: 1
+	  }),
+	  layers: [
+	    new ol.layer.Tile({
+	      source: new ol.source.MapQuest({layer: 'osm'})
+	    })
+	  ],
+	  target: this._content.domNode
+	});
+}, {
+	height: function(h) {
+		if (arguments.length) {
+			this._content.height(h);
+			this.olMap.updateSize();
+		} else {
+			return this._content.height();
+		}
+	},
+	width: function(w) {
+		if (arguments.length) {
+			this._content.width(w);
+			this.olMap.updateSize();
+		} else {
+			return this._content.width();
+		}
+	}
+});
 
 /**
 @params args {
@@ -12,5 +48,8 @@ var Label = require('absolute/Label');
 */
 module.exports = compose(_ContentDelegate, function(args) {
 	this._args = args;
-	this._content = new Label().value('map of '+args.modelId);
+	this._content = new VFlex([
+		[new Label().value('map of '+ args.modelId).height(20), 'fixed'],
+		this._map = new Map()
+	]);
 });
