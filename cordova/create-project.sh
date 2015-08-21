f9dd04adf0356189fae62399866912b79f2142c7
Quentin Vuilliot <quentin.vuilliot@kaes.fr>
2015-08-21 16:47:30 +0200

    Changed name & icon of app

--- cordova/create-project.sh
+++ cordova/create-project.sh
@@ -1,6 +1,6 @@
 cordova create project fr.bef.tryton "Tryton Mobile" --copy-from=www
 cp config.xml project/config.xml
-#cp -r icons project/
+cp -r icons project/
 cd project/
 cordova plugin add cordova-plugin-whitelist
 cordova plugin add https://github.com/ffournier/cordova-plugin-mbtiles.git
