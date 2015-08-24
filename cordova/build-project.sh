c3a235a6f3363d426d2f73c1dd2fc93490739caf
Quentin Vuilliot <quentin.vuilliot@kaes.fr>
2015-08-24 19:39:53 +0200

    Builds crosswalk x86 & arm7

--- cordova/build-project.sh
+++ cordova/build-project.sh
@@ -6,5 +6,7 @@ cp -r ../www/* www/
 cordova build android --release
 cd ..
 
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore release.keystore project/platforms/android/build/outputs/apk/android-release-unsigned.apk tryton
-zipalign -f -v 4 project/platforms/android/build/outputs/apk/android-release-unsigned.apk Tryton.apk
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore release.keystore project/platforms/android/build/outputs/apk/android-armv7-release-unsigned.apk tryton
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore release.keystore project/platforms/android/build/outputs/apk/android-x86-release-unsigned.apk tryton
+zipalign -f -v 4 project/platforms/android/build/outputs/apk/android-armv7-release-unsigned.apk SIME-arm7.apk
+zipalign -f -v 4 project/platforms/android/build/outputs/apk/android-x86-release-unsigned.apk SIME-x86.apk
